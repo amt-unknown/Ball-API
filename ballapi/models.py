1bca2dae1ba51ad583a1080d89cb7300382da6f2
Andrew Thompson <amtunknown@Andrews-MBP.lan>
2022-07-16 13:36:22 -0700

    Added reptile model, get/put routes and get a specfic id route

--- ballapi/models.py
+++ ballapi/models.py
@@ -7,6 +7,9 @@ class Reptile(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     common_name = db.Column(db.String(250))
-    sci_name = db.Column(db.String(250))
-    habitat = db.Column(db.String(250))
+    scientific_name = db.Column(db.String(250))
+    conservation_status = db.Column(db.String(250))
+    native_habitat = db.Column(db.String(250))
     fun_fact = db.Column(db.Text)
+
+        
--- ballapi/reptiles.py
+++ ballapi/reptiles.py
@@ -1,12 +1,52 @@
-from flask import (Blueprint, request)
+from concurrent.futures.process import _chain_from_iterable_of_lists
+from operator import concat
+from flask import (Blueprint, request, redirect, render_template, jsonify)
 from flask_sqlalchemy import SQLAlchemy
+from . import models
 
 bp = Blueprint('reptile', __name__, url_prefix="/reptiles")
+
+def create_dict(obj):
+    return {
+            'id': obj.id,
+            'common_name': obj.common_name,
+            'scientific_name': obj.scientific_name,
+            'native_habitat': obj.native_habitat,
+            'conservation_status': obj.conservation_status, 
+            'fun_fact': obj.fun_fact
+
+        }
+
+def create_dict_list(lst):
+    temp_lst = []
+    for data in lst:
+        temp_lst.append(create_dict(data))
+
+    return temp_lst
+
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        print(request.form)
-        return 'POST /reptiles'
-    else:
-        return 'GET /reptiles'
+        common_name = request.form['common_name']
+        scientific_name = request.form['scientific_name']
+        native_habitat = request.form['native_habitat']
+        conservation_status = request.form['conservation_status']
+        fun_fact = request.form['fun_fact']
+
+        new_reptile = models.Reptile(common_name=common_name, scientific_name=scientific_name, native_habitat=native_habitat, conservation_status=conservation_status, fun_fact=fun_fact)
+        models.db.session.add(new_reptile)
+        models.db.session.commit()
+
+        return redirect('/reptiles')
+    
+    results = models.Reptile.query.all()
+
+
+    return jsonify(create_dict_list(results))
+
+@bp.route('/<int:id>', methods=['GET'])
+def show(id):
+    reptile = models.Reptile.query.filter_by(id=id).first()
+
+    return create_dict(reptile)
